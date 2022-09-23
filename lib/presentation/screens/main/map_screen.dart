23d3289381520e0837eae02f729fc44dc67db3d5
kyle reginaldo <97393306+KyleReginaldo@users.noreply.github.com>
2022-09-23 12:23:53 +0800

    mapbox implement

--- lib/presentation/screens/main/map_screen.dart
+++ lib/presentation/screens/main/map_screen.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:general/general.dart';
+import 'package:latlong2/latlong.dart';
+
+class Hazard {
+  final String templateUrl;
+  final String title;
+
+  Hazard({required this.templateUrl, required this.title});
+}
+
+const floodUrl =
+    'https://api.mapbox.com/styles/v1/dankz01/cl8dwollz003n14mvp6oxul1d/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGFua3owMSIsImEiOiJjbDU2Zzl1NTAwYzl0M2luYmRkNjZoMHlpIn0.-hCnY7ZjwBW0Wf91zheEPg';
+const landslideUrl =
+    'https://api.mapbox.com/styles/v1/dankz01/cl8dwxk5l000515rkr74pah51/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGFua3owMSIsImEiOiJjbDU2Zzl1NTAwYzl0M2luYmRkNjZoMHlpIn0.-hCnY7ZjwBW0Wf91zheEPg';
+const stormUrl =
+    'https://api.mapbox.com/styles/v1/dankz01/cl8dwzr4g001h14nsoiamysoa/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGFua3owMSIsImEiOiJjbDU2Zzl1NTAwYzl0M2luYmRkNjZoMHlpIn0.-hCnY7ZjwBW0Wf91zheEPg';
+List<Hazard> dropdown = [
+  Hazard(templateUrl: floodUrl, title: 'Flood'),
+  Hazard(templateUrl: landslideUrl, title: 'Landslide'),
+  Hazard(templateUrl: stormUrl, title: 'Storm Surge'),
+];
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,20 +30,37 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Hazard template = dropdown.first;
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: MapOptions(),
+      options: MapOptions(center: LatLng(9.239878, 126.024329)),
       nonRotatedChildren: [
         TileLayer(
-          urlTemplate:
-              'https://api.mapbox.com/styles/v1/dankz01/cl8b5fez5007n14qlk1sz2x1w/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGFua3owMSIsImEiOiJjbDU2Zzl1NTAwYzl0M2luYmRkNjZoMHlpIn0.-hCnY7ZjwBW0Wf91zheEPg',
+          urlTemplate: template.templateUrl,
+          fastReplace: true,
           additionalOptions: const {
             'accessToken':
                 'pk.eyJ1IjoiZGFua3owMSIsImEiOiJjbDU2Zzl1NTAwYzl0M2luYmRkNjZoMHlpIn0.-hCnY7ZjwBW0Wf91zheEPg',
             'id': 'mapbox.mapbox-streets-v8'
           },
         ),
+        Center(
+          child: DropdownButton<Hazard>(
+              value: template,
+              items: dropdown.map((e) {
+                return DropdownMenuItem<Hazard>(
+                  value: e,
+                  child: CustomText(e.title),
+                );
+              }).toList(),
+              onChanged: (Hazard? val) {
+                setState(() {
+                  template = val!;
+                });
+              }),
+        )
         // MarkerLayer(
         //   markers: [
         //     Marker(
